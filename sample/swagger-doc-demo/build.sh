79d950e46f10a3145767c43b65f87c2b6574b940
leeokdkrin5l <weichal982119@yeah.net>
2021-07-03 15:14:52 +0800

    完善demo

--- sample/swagger-doc-demo/build.sh
+++ sample/swagger-doc-demo/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 mvn clean -U process-resources package -Dmaven.test.skip=true
-cp target/swagger-doc-demo-1.0-SNAPSHOT.jar  source
+mkdir source
+cp target/swagger-doc-demo-1.0-SNAPSHOT-sources.jar  source
