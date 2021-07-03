608fae2bce905cd392ac65b6de8490623c255f7f
leeokdkrin5l <weichal982119@yeah.net>
2021-07-03 16:46:52 +0800

    完善demo

---
+++ sample/swagger-doc-demo/src/main/java/com/swagger/demo/FilterInterceptor.java
@@ -0,0 +1,37 @@
+package com.swagger.demo;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wk
+ * Date: 2017-07-13 下午6:05
+ */
+public class FilterInterceptor implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+
+    public void destroy() {
+
+    }
+}
--- sample/swagger-doc-demo/src/main/java/com/swagger/demo/SampleApplication.java
+++ sample/swagger-doc-demo/src/main/java/com/swagger/demo/SampleApplication.java
@@ -2,7 +2,12 @@ package com.swagger.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.Arrays;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,14 +15,26 @@ import org.springframework.context.ConfigurableApplicationContext;
  * Date: 2017-07-13 下午5:19
  */
 @SpringBootApplication
-public class SampleApplication  {
+public class SampleApplication extends WebMvcConfigurationSupport {
     private static ConfigurableApplicationContext configurableApplicationContext;
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         SpringApplication springApplication = new SpringApplication(SampleApplication.class);
         configurableApplicationContext = springApplication.run(args);
 
     }
+
     public static ConfigurableApplicationContext getInstalce() {
         return configurableApplicationContext;
+    }
+
+    @Bean
+    public FilterRegistrationBean logFilterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new FilterInterceptor());
+        filterRegistrationBean.setName("logFilterRegistrationBean");
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
     }
 }
